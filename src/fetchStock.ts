6cc8b6bf9b18c5e9298cf143037ecb09a9d758f2
s1f10210368 <s1f102103684@iniad.org>
2023-06-26 20:12:28 +0900

    ch

--- src/fetchStock.ts
+++ src/fetchStock.ts
@@ -1,22 +1,38 @@
-import { chromium } from "playwright";
+import { chromium, ChromiumBrowser, ChromiumBrowserContext, Page } from 'playwright';
 
-export async function fetchStock(stockCode: string): Promise<number> {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+async function fetchStockPrice(url: string): Promise<string> {
+  let browser: ChromiumBrowser;
+  let context: ChromiumBrowserContext;
+  let page: Page;
 
-  // 適切な株価情報サイトへアクセスし、株価コードを入力して検索
-  // 以下は仮のURLとセレクターです。実際のサイトに合わせて変更してください。
-  await page.goto("https://example.com/stock");
-  await page.type("#search-box", stockCode);
-  await page.click("#search-button");
+  try {
+    browser = await chromium.launch();
+    context = await browser.newContext();
+    page = await context.newPage();
 
-  // 取得した株価情報を取得し、数値に変換
-  const stockPrice = await page.$eval("#stock-price", el => parseFloat(el.textContent));
+    await page.goto(url);
 
-  // ブラウザを閉じて結果を返す
-  await browser.close();
-  return stockPrice;
+    // 任意の株価の値が入っている要素のセレクタを指定する
+    const stockPriceSelector = '<CSS_SELECTOR>';
+
+    const stockPrice = await page.$eval(stockPriceSelector, (price) => {
+      return price.textContent.trim();
+    });
+
+    return stockPrice;
+  } catch (error) {
+    console.error(error);
+    return 'Error occurred';
+  } finally {
+    await page?.close();
+    await context?.close();
+    await browser?.close();
+  }
 }
 
-アイウエオアイウエオ
+(async () => {
+  // 株価情報を取得したいウェブサイトのURLを指定する
+  const targetUrl = '<WEBSITE_URL_WITH_STOCK_PRICE>';
+  const stockPrice = await fetchStockPrice(targetUrl);
+  console.log(`Stock price: ${stockPrice}`);
+})();

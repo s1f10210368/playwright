9c72cf7af8ba7aedbdff46a868c23a3b70cafa73
s1f10210368 <s1f102103684@iniad.org>
2023-06-27 18:56:50 +0900

    W

--- src/fetchStock.ts
+++ src/fetchStock.ts
@@ -1,23 +1,34 @@
 import { chromium } from 'playwright';
 
-export async function fetchStock(stockSymbol: string): Promise<number> { // Promise<number> を追加
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  const url = `https://finance.yahoo.com/quote/${stockSymbol}`;
+export async function fetchStock(stockSymbol: string): Promise<number> {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    const url = `https://finance.yahoo.co.jp/quote/${stockSymbol}`;
 
-  await page.goto(url);
+    await page.goto(url);
 
-  const stockHeaderInfo = await page.waitForSelector('[data-test="quote-header-info"]');
-  const stockPriceElement = await stockHeaderInfo.$('h1+div>span:first-child'); // セレクタを修正
-  if (stockPriceElement === null) { 
-    throw new Error("Stock price element not found");
-  }
-  const stockPrice = await stockPriceElement.textContent();
-  if (stockPrice === null) {
-    throw new Error("Stock price text not found");
-  }
+    const stockParentElement = await page.waitForSelector('[data-etfw-key="quotePrice"]'); // changed selector
+    if (stockParentElement === null) {
+        throw new Error("Stock price parent element not found");
+    }
 
-  await browser.close();
+    const stockPriceElement = await stockParentElement.$(
+        '._2ikyOH > span'
+    ); // changed selector
+    if (stockPriceElement === null) {
+        throw new Error("Stock price element not found");
+    }
+    const stockPrice = await stockPriceElement.textContent();
+    if (stockPrice === null) {
+        throw new Error("Stock price text not found");
+    }
 
-  return Number(stockPrice.trim().replace(/,/g, ''));
+    await browser.close();
+
+    return Number(stockPrice.trim().replace(/,/g, ""));
 }
+
+// スクリプトの最後にこの行を追加
+fetchStock("7203.T").then((stockPrice) => {
+    console.log("Toyota's stock price:", stockPrice);
+});
